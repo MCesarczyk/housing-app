e0ffaa39bc3e1ccff6318a9aaa2026bef71116f4
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-05-21 21:08:42 +0200

    feat(detail): pass route prop to component

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [],
-  template: `
-    <p>
-      details works!
-    </p>
-  `,
+  template: `<p>details works! {{ housingLocationId }}</p>`,
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
-
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = -1;
+  constructor() {
+    this.housingLocationId = this.route.snapshot.params['id'];
+  }
 }
