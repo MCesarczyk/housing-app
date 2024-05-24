ecd3e97e477daaf82d0eb3ad19b52fc96711ad54
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-05-25 01:28:12 +0200

    feat(housing-service): add error handling

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { HousingLocation } from './housinglocation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HousingService {
-  private url = 'api/locations';
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
@@ -16,12 +15,29 @@ export class HousingService {
     private http: HttpClient
   ) { }
 
+  private url = 'api/locations';
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      console.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
+
   getAllHousingLocations(): Observable<HousingLocation[]> {
-    return this.http.get<HousingLocation[]>(this.url);
+    return this.http.get<HousingLocation[]>(this.url).pipe(catchError(this.handleError<HousingLocation[]>('getAllHousingLocations', [])));
+  }
+
+  getLoationNo404(id: number): Observable<HousingLocation> {
+    const url = `${this.url}/?id=${id}`;
+    return this.http.get<HousingLocation[]>(url).pipe(
+      map(location => location[0]), catchError(this.handleError<HousingLocation>(`getHousingLocation id=${id}`))
+    );
   }
 
   getHousingLocationById(id: number): Observable<HousingLocation | undefined> {
-    return this.http.get<HousingLocation>(`${this.url}/${id}`);
+    return this.http.get<HousingLocation>(`${this.url}/${id}`).pipe(catchError(this.handleError<HousingLocation>(`getHousingLocationById id=${id}`)));
   }
 
   submitApplication(firstName: string, lastName: string, email: string) {
